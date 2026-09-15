4e647f1f21cc259dd14c5b7c184f6fbe62063328
Hung Ngo <hungnln2102@gmail.com>
2026-09-15 22:54:36 +0700

    refactor: remove unused file

--- backend/temp_query.sql
+++
@@ -1,5 +0,0 @@
-
-SELECT id, credit_code, customer_name, source_order_code, available_amount, status
-FROM billing.refund_credit_notes
-WHERE available_amount > 0 AND status IN ('OPEN', 'PARTIALLY_APPLIED')
-ORDER BY id DESC;
